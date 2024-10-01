59faac526bc58b05e9ae18f106a15709f81265c5
EliasderMessias <57588829+EliasderMessias@users.noreply.github.com>
2024-10-01 20:25:46 +0200

    -Added src folder with empty init file and file  for k-nearest-neighbour algorithm (similar to scikitlearns version) -functionality includes fitting and predicting for different k

---
+++ ML_Python/src/knn.py
@@ -0,0 +1,49 @@
+import numpy as np
+from collections import Counter
+
+def euclid_dist(x,y):
+    return np.sqrt(np.sum((x-y)**2))
+
+class KNN:   #class that mirrors functionality of scikitlearns ML methods for k-Nearest-Neighbours
+    def __init__(self, k = 3):
+        if k !=3 and k > 0:
+            self.k = k
+        elif k <= 0:
+            raise ValueError("Neighbourparameter cannot be <= 0")
+        else:
+            pass
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+    
+    def predict(self, X):
+        prediction = [self._predict(x) for x in X]
+        return np.array(prediction)
+    
+    def _predict(self, x):
+        #calculate distance to all points in X_train
+        distances = [euclid_dist(x,x_train) for x_train in self.X_train]   
+
+        #sort for shortest distance and take array of the k-first
+        indices = np.argsort(distances)[:self.k]
+
+        #give the labels for classification from the indices of the previous step
+        labels = [self.y_train[i] for i in indices]
+
+        #make a decision by majority of occuring labels
+        predicted_label = Counter(labels).most_common(1)
+
+        return predicted_label
+
+
+
+
+
+        
+        
+
+
+   
+
+
