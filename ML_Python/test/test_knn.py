68da0738b2974df31adfc394ee695c39ca86c361
EliasderMessias <57588829+EliasderMessias@users.noreply.github.com>
2024-10-03 15:49:28 +0200

    Rewrote Testing for KNN class with unittest, 3 tests for basic functionality

---
+++ ML_Python/test/test_knn.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+import sys
+path_root = Path(__file__).parents[1]
+sys.path.append(str(path_root))
+
+import unittest
+import numpy as np
+from src.knn import KNNClassifier, KNNRegressor
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+
+class TestKNN(unittest.TestCase):
+    def test_internalNeighbourParam(self):
+        self.assertEqual(KNNClassifier(3).k, 3)
+        self.assertEqual(KNNClassifier(1).k, 1)
+        with self.assertRaises(ValueError):
+            KNNClassifier(-1)
+    
+    def test_KNNClassifier(self):
+        iris_dataset = load_iris()
+        X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'],iris_dataset['target'], random_state=0)    
+        knn = KNNClassifier(3)
+        knn.fit(X_train,y_train)
+        #testing against scikits prediction 
+        self.assertTrue((knn.predict(X_test) == [2,1,0,2,0,2,0,1,1,1,2,1,1,1,1,0,1,1,0,0,2,1,0,0,2,0,0,1,1,0,2,1,0,2,2,1,0,2]).all()) 
+
+    def test_KNNRegressor(self):
+        X_train = np.array([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]])
+        y_train = np.array([2, 3, 4, 5, 6])
+        X_test = np.array([[2.5, 3.5], [4.5, 5.5]])
+
+        knn = KNNRegressor(2)
+        knn.fit(X_train,y_train)
+        self.assertTrue((knn.predict(X_test) == [3.5,5.5]).all())
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
+
+
